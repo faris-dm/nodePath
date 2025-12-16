17e355b2c77d86a7c058535425e10183e56993d6
faris Naser <solonaser9@gmail.com>
2025-12-15 16:44:29 -0800

    add some packages  and info for better useage in Day1

--- Day1/index.js
+++ Day1/index.js
@@ -1,10 +1,21 @@
+// import http from "http";
+
+// const app = http.createServer((req, res) => {
+//   res.writeHead(200, { "content-type": "text/plain" });
+//   res.end("hellow world Node  with Ai Mentor");
+// });
+
+// app.listen(3000, () => {
+//   console.log(` server running at https://localhost:3000`);
+// });
+
+// Task Two
 import http from "http";
-
 const app = http.createServer((req, res) => {
-  res.writeHead(200, { "content-type": "text/plain" });
-  res.end("hellow world Node  with Ai Mentor");
+  res.end(" solo naser  pleases serve  your ");
 });
+const port = 8000;
 
-app.listen(3000, () => {
-  console.log(` server running at https://localhost:3000`);
+app.listen(port, () => {
+  console.log(`https://localhost:${port}`);
 });
