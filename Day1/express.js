b348b2ba38db8acae31e590c82f263715c187f53
faris Naser <solonaser9@gmail.com>
2025-12-26 17:47:45 -0800

    feat:add aroute to accept req,res in server

--- Day1/express.js
+++ Day1/express.js
@@ -1,8 +1,31 @@
 import express from "express";
 const app = express();
+app.use(express.json());
+const items = [
+  { id: 1, name: "faris naser" },
+  { id: 2, name: "Faysel naser" },
+  { id: 3, name: "Fuad naser" },
+];
 
 app.get("/", (req, res) => {
-  res.send(" Landin Pages");
+  res.statusCode = 200;
+  res.end(JSON.stringify({ message: "Welcome to Landing Page" }));
+});
+app.get("/home", (req, res) => {
+  res.statusCode = 200;
+  res.end(JSON.stringify({ HomePage: " welcome to home pages" }));
+});
+app.get("/items", (req, res) => {
+  res.statusCode = 200;
+  res.end(JSON.stringify({ message: items }));
+});
+app.get("/items/:id", (req, res) => {
+  const ids = items.find((c) => c.id === parseInt(req.params.id));
+  if (!ids) {
+    res.status(404).send(" the item does not found");
+  } else {
+    res.send(ids);
+  }
 });
 
 app.listen(5400, () => {
--- Day1/server.js
+++ Day1/server.js
@@ -1,97 +1,110 @@
-import { fileLoader } from "ejs";
-import { json } from "express";
+// import { fileLoader } from "ejs";
+// import { json } from "express";
 
-// console.log(" the privious file:-", fs.readFileSync("fs.txt", "utf-8"));
+// // console.log(" the privious file:-", fs.readFileSync("fs.txt", "utf-8"));
 
-// fs.appendFile("fs.txt", "solo is wrote", (error, data) => {
-//   if (error) throw error;
-//   console.log(" the new file:", fs.readFileSync("fs.txt", "utf-8"));
+// // fs.appendFile("fs.txt", "solo is wrote", (error, data) => {
+// //   if (error) throw error;
+// //   console.log(" the new file:", fs.readFileSync("fs.txt", "utf-8"));
+// // });
+
+// // console.log(" reading the file", fs.readFileSync("fs.txt", "utf-8"));
+// // fs.appendFile("fs.txt", "and i will change  them to wins", (error) => {
+// //   if (error) throw error;
+// //   console.log(fs.readFileSync("fs.txt", "utf-8"));
+// // });
+// // import fs from "fs";
+
+// // This file exists
+// // const file = "fs.txt";
+
+// // const ErrorFirstCallback = (err, data) => {
+// //   if (err) throw err;
+
+// //   console.log("Function successfully executed:", data.toString());
+// // };
+
+// // fs.readFile(file, ErrorFirstCallback);import { unlink } from 'node:fs/promises';
+
+// // const file = "fs.txt";
+
+// // fs.appendFileSync(file, "\n You are Gc Pleases Work Hard");
+
+// // const out = fs.readFileSync(file, "utf-8");
+// // console.log(" this is a file", out.toString())
+// import fs, { readFile, readFileSync } from "fs";
+
+// import http from "http";
+
+// const logFile = "requests.log";
+
+// const server = http.createServer((req, res) => {
+//   const timestamp = new Date().toISOString();
+//   const { method, url } = req;
+
+//   // Log the request (without the extra route-specific messages)
+//   const logMessage = `${timestamp} - ${method} ${url}\n`;
+
+//   // Asynchronously append to the log file
+//   fs.appendFile(logFile, logMessage, (err) => {
+//     if (err) {
+//       console.error("Failed to write to log file:", err);
+//     }
+//   });
+
+//   res.setHeader("Content-Type", "application/json");
+
+//   let responseMessage;
+//   let statusCode;
+
+//   if (url === "/") {
+//     statusCode = 200;
+//     responseMessage = { message: "Landing Page" };
+//   } else if (url === "/about") {
+//     statusCode = 200;
+//     responseMessage = { message: "ABOUT ME PAGE" };
+//   } else if (url === "/product") {
+//     statusCode = 200;
+//     responseMessage = { message: "product Page" };
+//   } else if (url === "/close") {
+//     statusCode = 200;
+//     responseMessage = { message: "Server is closing" };
+
+//     // Send response add . then close the server
+//     res.statusCode = statusCode;
+//     res.end(JSON.stringify(responseMessage));
+
+//     // Close the server
+//     server.close(() => {
+//       console.log("Server closed");
+//     });
+
+//     // We return early because we already sent the response and are closing the server.
+//     return;
+//   } else {
+//     statusCode = 404;
+//     responseMessage = { message: "Page not found" };
+//   }
+
+//   res.statusCode = statusCode;
+//   res.end(JSON.stringify(responseMessage));
 // });
 
-// console.log(" reading the file", fs.readFileSync("fs.txt", "utf-8"));
-// fs.appendFile("fs.txt", "and i will change  them to wins", (error) => {
-//   if (error) throw error;
-//   console.log(fs.readFileSync("fs.txt", "utf-8"));
+// const PORT = 4000;
+
+// server.listen(PORT, () => {
+//   console.log(`Server running at http://localhost:${PORT}`);
 // });
-// import fs from "fs";
 
-// This file exists
-// const file = "fs.txt";
+import expres from "express";
 
-// const ErrorFirstCallback = (err, data) => {
-//   if (err) throw err;
-
-//   console.log("Function successfully executed:", data.toString());
-// };
-
-// fs.readFile(file, ErrorFirstCallback);import { unlink } from 'node:fs/promises';
-
-// const file = "fs.txt";
-
-// fs.appendFileSync(file, "\n You are Gc Pleases Work Hard");
-
-// const out = fs.readFileSync(file, "utf-8");
-// console.log(" this is a file", out.toString())
-import fs, { readFile, readFileSync } from "fs";
-
-import http from "http";
-
-const logFile = "requests.log";
-
-const server = http.createServer((req, res) => {
-  const timestamp = new Date().toISOString();
-  const { method, url } = req;
-
-  // Log the request (without the extra route-specific messages)
-  const logMessage = `${timestamp} - ${method} ${url}\n`;
-
-  // Asynchronously append to the log file
-  fs.appendFile(logFile, logMessage, (err) => {
-    if (err) {
-      console.error("Failed to write to log file:", err);
-    }
-  });
-
-  res.setHeader("Content-Type", "application/json");
-
-  let responseMessage;
-  let statusCode;
-
-  if (url === "/") {
-    statusCode = 200;
-    responseMessage = { message: "Landing Page" };
-  } else if (url === "/about") {
-    statusCode = 200;
-    responseMessage = { message: "ABOUT ME PAGE" };
-  } else if (url === "/product") {
-    statusCode = 200;
-    responseMessage = { message: "product Page" };
-  } else if (url === "/close") {
-    statusCode = 200;
-    responseMessage = { message: "Server is closing" };
-
-    // Send response add . then close the server
-    res.statusCode = statusCode;
-    res.end(JSON.stringify(responseMessage));
-
-    // Close the server
-    server.close(() => {
-      console.log("Server closed");
-    });
-
-    // We return early because we already sent the response and are closing the server.
-    return;
-  } else {
-    statusCode = 404;
-    responseMessage = { message: "Page not found" };
-  }
-
-  res.statusCode = statusCode;
-  res.end(JSON.stringify(responseMessage));
+const app = expres();
+app.get("/", (req, res) => {
+  res.send(" hi this is home page");
 });
 
-const PORT = 4000;
+const port = 6001;
 
-server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
 });
